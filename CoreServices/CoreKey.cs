ef45193eb7a16b26abadec5cdaf9897d5c8381de
Carl Evans <carlgevans@evfam.net>
2014-03-28 18:25:16 +0000

    Created core services project shell.

---
+++ CoreServices/CoreKey.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace CoreServices
+{
+    public class CoreKey : ICoreKey
+    {
+        public string PrivateKey { get; set; }
+        public string PublicKey { get; set; }
+        public string PublicKeyHex
+        {
+            get
+            {
+                Byte[] stringBytes = Encoding.Default.GetBytes(this.PublicKey);
+                StringBuilder sbBytes = new StringBuilder(stringBytes.Length * 2);
+
+                foreach (byte b in stringBytes)
+                {
+                    sbBytes.AppendFormat("{0:X2}", b);
+                }
+
+                return sbBytes.ToString();
+            }
+        }
+
+        public CoreKey()
+        {
+            this.PrivateKey = string.Empty;
+            this.PublicKey = string.Empty;
+        }
+
+        public CoreKey(string privateKey)
+        {
+            this.PrivateKey = privateKey;
+        }
+
+        /// <summary>
+        /// This method generates a new ECDSA public and private key pair.
+        /// </summary>
+        public void Generate()
+        {
+            // Generate an ECDSA key pair.
+        }
+    }
+}
---
+++ CoreServices/ICoreKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CoreServices
+{
+    public interface ICoreKey
+    {
+        string PrivateKey { get; set; }
+        string PublicKey { get; set; }
+        string PublicKeyHex { get; set; }
+
+        /// <summary>
+        /// This method generates a new ECDSA public and private key pair.
+        /// </summary>
+        void Generate();
+    }
+}
